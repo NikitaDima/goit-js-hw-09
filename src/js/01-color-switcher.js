b4c0218e6024172989701c62baf24fdd724bbd80
NikitaDima <onekill13@gmail.com>
2023-02-28 01:57:37 +0200

    complet task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
+let isActiv = false;
+buttonStart.addEventListener('click', () => {
+  if (isActiv) {
+    return;
+  }
+  timerId = setInterval(() => {
+    isActiv = true;
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  isActiv = false;
+});
