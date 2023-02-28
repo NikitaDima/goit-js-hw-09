25637593e49f2d8e71620ba026c36bb209a78094
NikitaDima <onekill13@gmail.com>
2023-02-28 02:48:38 +0200

    add styles timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,27 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const buttonStart = document.querySelector('button[data-start]');
+const dataInput = document.querySelector('#datetime-picker');
+flatpickr(dataInput, {});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
